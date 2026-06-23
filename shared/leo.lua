67df9b8419c5d66663f6e779a9671e86baee49f4
vividresources <resources.vivid@gmail.com>
2026-06-23 14:41:51 +1000

    Delete shared directory

--- shared/leo.lua
+++
@@ -1,9 +0,0 @@
-function IsLeoJob(jobName)
-    if not jobName then return false end
-    for i = 1, #Config.PoliceJobs do
-        if Config.PoliceJobs[i] == jobName then
-            return true
-        end
-    end
-    return false
-end
